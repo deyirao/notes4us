723a3bbfde11c7a7c1eae6d99549a3ebd4cba15e
Deyi <deyi.rao@gmx.de>
2025-05-14 17:07:14 +0200

    Initialized spring boot project

---
+++ src/main/java/de/fourchads/notes4us/Notes4usApplication.java
@@ -0,0 +1,13 @@
+package de.fourchads.notes4us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Notes4usApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Notes4usApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fourchads/notes4us/Notes4usApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fourchads.notes4us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Notes4usApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
